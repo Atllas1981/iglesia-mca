646e9fd7939fe805b8b3468bb21b87bee7f668a9
Atllas1981 <samuelsanchez.rueda@gmail.com>
2026-04-26 10:54:34 -0500

    fix: named import y activación de CDN para blindaje

--- src/lib/sanity.ts
+++ src/lib/sanity.ts
@@ -1,10 +1,10 @@
 import { createClient } from '@sanity/client';
-import createImageUrlBuilder from '@sanity/image-url';
+import { createImageUrlBuilder } from '@sanity/image-url'; // Con llaves
 
 export const client = createClient({
   projectId: import.meta.env.PUBLIC_SANITY_PROJECT_ID,
   dataset: import.meta.env.PUBLIC_SANITY_DATASET,
-  useCdn: false, 
+  useCdn: true, // Activado para ahorro
   apiVersion: '2024-04-25',
 });
 
