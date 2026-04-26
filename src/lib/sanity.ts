3c9c07d621433922bdc247382d812ec01221077e
Atllas1981 <samuelsanchez.rueda@gmail.com>
2026-04-26 10:57:27 -0500

    fix: fallback de variables para despliegue en cloudflare

--- src/lib/sanity.ts
+++ src/lib/sanity.ts
@@ -1,10 +1,14 @@
 import { createClient } from '@sanity/client';
-import { createImageUrlBuilder } from '@sanity/image-url'; // Con llaves
+import { createImageUrlBuilder } from '@sanity/image-url';
+
+// Esta función asegura que obtengamos el ID sin importar dónde corra el código
+const projectId = import.meta.env.PUBLIC_SANITY_PROJECT_ID || "nmqh8k0r";
+const dataset = import.meta.env.PUBLIC_SANITY_DATASET || "production";
 
 export const client = createClient({
-  projectId: import.meta.env.PUBLIC_SANITY_PROJECT_ID,
-  dataset: import.meta.env.PUBLIC_SANITY_DATASET,
-  useCdn: true, // Activado para ahorro
+  projectId: projectId,
+  dataset: dataset,
+  useCdn: true, 
   apiVersion: '2024-04-25',
 });
 
